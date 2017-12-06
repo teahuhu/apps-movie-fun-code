12af0a6c648b2c81eb418f86e80d6aabf714e549
Brian Olecki <bolecki@travelers.com>
2017-12-06 09:12:15 -0500

    Basic spring boot app

---
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -0,0 +1,20 @@
+package org.superbiz.moviefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getSrvletBean(ActionServlet actionServlet) {
+        return new ServletRegistrationBean(actionServlet, "/moviefun/*");
+    }
+}
